7877d54cf734ad6776750b16077896e5954b5862
MABAIDUL HOQUE <132288979+Mabaidul-Hoque@users.noreply.github.com>
2024-03-30 13:20:20 +0530

    blog create , update and delete functionality added

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -31,11 +31,147 @@ const createBlog = async (req, res) => {
 };
 
 const updateBlog = async (req, res) => {
-  res.status(200).json({ message: "success" });
+  const blogId = req.params.id;
+  const userId = req.userId;
+  try {
+    // Find the blog to update
+    const blogToUpdate = await Blog.findById(blogId);
+    if (!blogToUpdate) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+    // Checking if the user is authorized to delete the blog
+    if (blogToUpdate.user.toString() !== userId) {
+      return res.status(403).json({
+        status: "failed",
+        message: "You are not authorized to delete this blog",
+      });
+    }
+
+    // Update the blog
+    const updatedBlog = await Blog.findByIdAndUpdate(blogId, req.body, {
+      new: true,
+    });
+    // Find the user who created the blog
+    const user = await User.findById(blogToUpdate.user);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    // Update the reference in the user's 'later' array
+    const index = user.later.indexOf(blogId);
+    if (index !== -1) {
+      user.later[index] = updatedBlog._id;
+      await user.save();
+    }
+
+    res
+      .status(200)
+      .json({ message: "Blog updated successfully", data: updatedBlog });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
 };
 
 const deleteBlog = async (req, res) => {
-  res.status(200).json({ message: "success" });
+  const blogId = req.params.id;
+  const userId = req.userId;
+  try {
+    // Find the blog to delete
+    const blogToDelete = await Blog.findById(blogId);
+    if (!blogToDelete) {
+      return res
+        .status(404)
+        .json({ status: "failed", message: "Blog not found" });
+    }
+    // Checking if the user is authorized to delete the blog
+    if (blogToDelete.user.toString() !== userId) {
+      return res.status(403).json({
+        status: "failed",
+        message: "You are not authorized to delete this blog",
+      });
+    }
+
+    // Delete the blog
+    const deletedBlog = await Blog.findByIdAndDelete(blogId);
+
+    // Find the user who created the blog
+    const user = await User.findById(blogToDelete.user);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: "failed", message: "User not found" });
+    }
+
+    // Remove the blog reference from the user's 'later' array
+    user.later.pull(blogId);
+    await user.save();
+
+    res.status(200).json({
+      status: "Success",
+      message: "Blog deleted successfully",
+      data: deletedBlog,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "failed",
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
 };
 
-module.exports = { createBlog, updateBlog, deleteBlog };
+const getAllBlogs = async (req, res) => {
+  try {
+    const data = await Blog.find();
+    if (!data) {
+      res
+        .status(404)
+        .json({ status: "failed", message: "Blog data not found!" });
+    }
+
+    res.status(200).json({
+      status: "Success",
+      message: `Found ${data.length} blog data`,
+      results: data.length,
+      data,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+const getOneBlog = async (req, res) => {
+  const blogId = req.params.id;
+  try {
+    const data = await Blog.findById(blogId).populate({
+      path: "user",
+      model: "users",
+    });
+    if (!data) {
+      res
+        .status(404)
+        .json({ status: "failed", message: "Blog data not found!" });
+    }
+
+    res.status(200).json({
+      status: "Success",
+      message: "Found blog data",
+      data,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+module.exports = {
+  createBlog,
+  updateBlog,
+  deleteBlog,
+  getAllBlogs,
+  getOneBlog,
+};
--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -3,13 +3,17 @@ const {
   createBlog,
   updateBlog,
   deleteBlog,
+  getAllBlogs,
+  getOneBlog,
 } = require("../controllers/blogController");
 const isUserValid = require("../middlewares/isUserValid");
 
 const router = express.Router();
 
 router.post("/create", isUserValid, createBlog);
-router.put("/:id", isUserValid, updateBlog);
-router.delete("/:id", isUserValid, deleteBlog);
+router.put("/update/:id", isUserValid, updateBlog);
+router.delete("/delete/:id", isUserValid, deleteBlog);
+router.get("/allblogs", getAllBlogs);
+router.get("/one/:id", getOneBlog);
 
 module.exports = router;
