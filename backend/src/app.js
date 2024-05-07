09ce2d52b45e62a73c770dadd6ec5b8f368d138c
Mabaidul-Hoque <mabaidulhoque95@gmail.com>
2024-05-07 19:17:03 +0530

    Frontend part createtion and checking for connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes/index");
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use("/api/v1/blogApp/", router);
